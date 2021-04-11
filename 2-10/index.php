6670f5a58b7bbfd01df884c2057128393a4ac06c
kenyaniino <80149513+kenyaniino@users.noreply.github.com>
2021-04-11 13:08:51 +0900

    課題
    お願いいたします

---
+++ 2-10/index.php
@@ -0,0 +1,13 @@
+<?php
+function getRectangular($vertical,$side,$height){
+    $area = $vertical * $side * $height;
+    echo $area;
+}
+getRectangular(5,10,8);
+
+?>
+<!-- ハッシュ化→元の値をハッシュ関数と呼ばれる値を中に放り込むと適当な値を返してくれる関数に入れてハッシュ値に変換すること 
+
+総合テスト→ソフトウェア、システムの検証方法
+
+デバッグ→バグを修正してくれるもの -->
