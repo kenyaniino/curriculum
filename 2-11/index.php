586b57cc0bde6955c64fd7edd15bb5a0fcb885af
kenyaniino <80149513+kenyaniino@users.noreply.github.com>
2021-04-12 22:53:00 +0900

    課題
    確認お願いします

---
+++ 2-11/index.php
@@ -0,0 +1,16 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="email">
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<!-- 
+仕様書・設計書→仕様書は何を作るか記したもので、設計書はどうやって作るかしめしたもの
+
+Git→プログラムのソースコードなどの変更履歴を管理する分散型バージョン管理システム
+
+マージツール→MDLファイル中のマップ式をテキスト形式で記述したファイルに出力する機能 -->
