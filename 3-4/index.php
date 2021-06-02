28c34769bcc784cd97e15e0f6302fab48aa5eba6
kenyaniino <80149513+kenyaniino@users.noreply.github.com>
2021-06-03 01:25:57 +0900

    課題
    確認お願いします

---
+++ 3-4/index.php
@@ -0,0 +1,58 @@
+<?php
+//作成したpdo.phpを読み込む
+// require_once('pdo.php');
+
+require_once('getData.php');
+
+$getData = new getData();
+     
+$users = $getData->getUserData();
+$posts = $getData->getPostData();
+
+var_dump($posts);//表示させる
+// 関数db_connect()からPDOを取得する
+// $pdo = db_connect();
+?>
+
+
+<!DOCTYPE html>
+<html lang="jp">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet">
+        <title>test</title>
+    </head>
+    <body>
+        <header>
+            <div class="left"><img src="imgs/logo.png" width="180px" height="100px"></div>
+            <div class="top">
+            <?php
+            $users = array("id" => "1", "first_name" => "翔伍", "last_name" => "隼田", "last_login" => "2021-05-31 21:54:52");
+            echo $users["id"];
+            echo $users["last_name"];
+            echo $users["first_name"];
+            //var_dump($users);表示させる
+            ?>
+            </div>
+            <div class="buttom">
+                <?php
+                    echo $users["last_login"];
+                ?>
+            </div>
+            <div>
+
+            
+        </div>
+    </header>
+    <main>
+        <div>
+        </div>
+    </main>
+    <footer>
+        <div>
+        </div>
+    </footer>
+</body>
+</html>
