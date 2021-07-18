b3cb7af57f06da5d8c4f2fc5dca8cd51dd6596fe
kenyaniino <80149513+kenyaniino@users.noreply.github.com>
2021-07-19 01:26:57 +0900

    課題
    確認お願いします

--- 4-1/signUp.php
+++ 4-1/signUp.php
@@ -7,40 +7,45 @@ require_once('db_connect.php');
 $name = $_POST['name'];
 $password = $_POST['password'];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
-
+// $messege = "";
+// $errorMessage = "";
 ?>
 
 
  
 <?php
+// session_start();
             // POSTで送られたデータがあった場合
             if (isset($_POST["signUp"])) {   //もしPOSTで送られてきたら、以下の処理を実行する
-                
-                // $messege = "登録が完了しました。";
-                // 処理
-                $pdo = db_connect();
-                try {
-                    
-                    // PDOインスタンスの作成
-                    // $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-                    // エラー処理方法の設定
-                    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
-                    // プリペアドステートメントの作成 
-                    $stmt = $pdo->prepare($sql);
-                    //値をセット
-                    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-                    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-                    // :passwordにバインドする場合は、$password_hashを使用する
-                    $stmt->bindValue(':password', $password_hash);
-                    $stmt->execute();
-                    
-                }
-                catch(PDOException $e) {
-                    echo "データベースエラー";
-                    echo $e->getMessage();
-                    die();
-                }
+                if (!empty($_POST['name']) && !empty($_POST['password'])){
+
+                    echo "登録が完了しました。";
+                    // echo htmlspecialchars($messege, ENT_QUOTES);
+                } 
+                    // 処理
+                    $pdo = db_connect();
+                    try {
+                        
+                        // PDOインスタンスの作成
+                        // $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+                        // エラー処理方法の設定
+                        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
+                        // プリペアドステートメントの作成 
+                        $stmt = $pdo->prepare($sql);
+                        //値をセット
+                        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+                        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+                        // :passwordにバインドする場合は、$password_hashを使用する
+                        $stmt->bindValue(':password', $password_hash);
+                        $stmt->execute();
+                        
+                    }
+                    catch(PDOException $e) {
+                        echo "データベースエラー";
+                        echo $e->getMessage();
+                        die();
+                    }
             }
                 
 ?>
